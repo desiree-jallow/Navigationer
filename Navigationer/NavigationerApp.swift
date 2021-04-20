1d2a0e9de0b685c11da1ae6abdeae1e2065412a9
Desiree Jallow <60536540+desiree-jallow@users.noreply.github.com>
2021-04-20 16:16:15 -0500

    Initial Commit

---
+++ Navigationer/NavigationerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationerApp.swift
+//  Navigationer
+//
+//  Created by Desiree on 4/20/21.
+//
+
+import SwiftUI
+
+@main
+struct NavigationerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
